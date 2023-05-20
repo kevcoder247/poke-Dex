67315f28e596a43b981a578111ae6ad045d7dda7
kevin ventura <kevcoder@kevins-mbp.myfiosgateway.com>
2023-05-19 20:15:44 -0400

    wip: form added, and user input can now be collected and used to search the API

--- js/app.js
+++ js/app.js
@@ -2,17 +2,34 @@ let pokemon;
 
 const $name = $("#name")
 const $img = $("img");
+const $input = $('input[type="text"]')
 
-$.ajax({
-  url:  'https://pokeapi.co/api/v2/pokemon/charmander'
-}).then(
-  (data) => {
-    pokemon = data
-    // console.log(pokemon)
-    console.log(pokemon.sprites.back_default)
-    console.log(pokemon.name)
-    
+//Event Listener
+$('form').on('submit', handleGetData);
+
+function handleGetData(event){
+  //Prevent page from reloading when we submit the user input
+  event.preventDefault();
+
+  //Get the user input
+  userInput = $input.val();
+
+  $.ajax({
+    url:  'https://pokeapi.co/api/v2/pokemon/' + userInput
+  }).then(
+    (data) => {
+      pokemon = data
+      render();
+    }, 
+    (error) => {
+      console.log('bad request', error)
+    }
+  )
+  
+  function render(){
     $name.text(pokemon.name)
     $img.attr('src', pokemon.sprites.front_default)
   }
-)
+}
+
+
